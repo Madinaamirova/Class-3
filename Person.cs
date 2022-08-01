97f1b404b6679e6e1272571060ea639cc4067bac
Madina <madiinaamiirova@gmail.com>
2022-08-02 03:23:53 +0400

    commit

---
+++ Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Person
+    {
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public bool IsMarried;
+        public Person()
+        {
+
+        }
+        public Person(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+        public Person(byte age, bool ismarried, string name, string surname) : this(name, surname)
+        {
+            Age = age;
+            IsMarried = ismarried;
+
+        }
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($@"Name --> {Name}
+            Surname --> {Surname}
+            Age --> {Age}
+             Is marry --> {IsMarried}");
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Student : Person
+    {
+        public string Degree;
+        public bool IsHigh;
+
+
+        public Student(string degree, bool isHigh, string name, string surname, byte age, bool ismarried) : base(age, ismarried, name, surname)
+        {
+            Degree = degree;
+            IsHigh = isHigh;
+        }
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($@"Degree --> {Degree}
+            Is high --> {IsHigh}");
+        }
+
+    }
+}
